cd21c3094464fb932a7ea4ca90b11b9a5a779936
Sérgio Neto <sergio.deusdedith@gmail.com>
2021-11-09 00:23:29 -0300

    feat: add endpoint to create and fetch people

--- nginx_node/index.js
+++ nginx_node/index.js
@@ -4,12 +4,18 @@ const bodyParser = require('body-parser')
 const port = 8080
 const { Sequelize } = require('sequelize')
 const { People } = require("./models/index")
+const { v4: uuidv4 } = require('uuid')
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', async (req, res) => {
+    res.send(await People.findAll())
+})
+
+app.post('/', async (req, res) => {
+    await People.create({ id: uuidv4(), name: req.body.name })
+    res.send(await People.findAll())
 })
 
 app.listen(port, () => {
