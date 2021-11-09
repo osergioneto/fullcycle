768909452fdfe71fb0b2a27daf8532f00819a2c2
Sérgio Neto <sergio.deusdedith@gmail.com>
2021-11-09 00:23:06 -0300

    feat: add bodyparser middleware

--- nginx_node/index.js
+++ nginx_node/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 8080
+const { Sequelize } = require('sequelize')
+const { People } = require("./models/index")
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
