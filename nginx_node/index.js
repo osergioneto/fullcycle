d534bb364c71b9d2dd4d912754ffa9bf0686647d
Sérgio Neto <sergio.deusdedith@gmail.com>
2021-11-11 22:37:38 -0300

    feat: add ejs and view to index

--- nginx_node/index.js
+++ nginx_node/index.js
@@ -9,13 +9,17 @@ const { v4: uuidv4 } = require('uuid')
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json())
 
+app.set('view engine', 'ejs');
+
 app.get('/', async (req, res) => {
-    res.send(await People.findAll())
+    const people = await People.findAll()
+    res.render('pages/index', { people: people })
 })
 
 app.post('/', async (req, res) => {
     await People.create({ id: uuidv4(), name: req.body.name })
-    res.send(await People.findAll())
+    const people = await People.findAll()
+    res.render('pages/index', { people: people })
 })
 
 app.listen(port, () => {
